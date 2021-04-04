796f7e4e965073129551cb0b93ec5df1b6afca47
Metarsit Leenayongwut <metarsit@outlook.com>
2021-04-04 23:53:40 +0800

    [1550] Three Consecutive Odds

---
+++ easy/1550/three_consecutive_odds_test.go
@@ -0,0 +1,38 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func threeConsecutiveOdds(arr []int) bool {
+	consecOdd := 0
+
+	for _, num := range arr {
+		if (num % 2) == 0 {
+			consecOdd = 0
+			continue
+		}
+
+		consecOdd += 1
+		if consecOdd == 3 {
+			return true
+		}
+	}
+	return false
+}
+
+func TestThreeConsecutiveOdds(t *testing.T) {
+	cases := []struct {
+		arr    []int
+		expect bool
+	}{
+		{[]int{2, 6, 4, 1}, false},
+		{[]int{1, 2, 34, 3, 4, 5, 7, 23, 12}, true},
+	}
+
+	for _, data := range cases {
+		assert.Equal(t, threeConsecutiveOdds(data.arr), data.expect)
+	}
+}
